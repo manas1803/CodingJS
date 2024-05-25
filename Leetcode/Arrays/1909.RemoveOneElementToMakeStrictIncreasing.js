c518116bf35285fae74c3765a9c61b389d197af2
manas1803 <manasmonupant@gmail.com>
2024-05-25 18:09:15 +0530

    Leetcode question on arrays

---
+++ Leetcode/Arrays/1909.RemoveOneElementToMakeStrictIncreasing.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canBeIncreasing = function (nums) {
+  debugger;
+  let count = 0;
+  for (let i = 1; i < nums.length && count < 2; i++) {
+    if (nums[i - 1] >= nums[i]) {
+      count++;
+    }
+    if (i > 1 && nums[i - 2] >= nums[i]) {
+      nums[i] = nums[i - 1];
+    }
+  }
+  return count < 2;
+};
+
+const canBeIncreasingVal = canBeIncreasing([1, 4, 1, 2, 3]);
+console.log(canBeIncreasingVal);
